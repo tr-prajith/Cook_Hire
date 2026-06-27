6a3cc60edcb6a568664f626d4b8b54b1f9b3b798
tr-prajith <prajithappu1357@gmail.com>
2026-06-27 20:31:21 +0530

    server updated

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use("/api/inquiry", inquiryRoute);
 app.use('/api/cooks',cookRoute)
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
     console.log(`Server Connected ${PORT}`);
